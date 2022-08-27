b8891c7bdf300c46a211a3d00e50737f41ede9f7
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-08-27 16:31:53 -0500

    subida

---
+++ app/src/main/java/com/uao/parcial1alarconjuan_sqliteappcrud/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uao.parcial1alarconjuan_sqliteappcrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
