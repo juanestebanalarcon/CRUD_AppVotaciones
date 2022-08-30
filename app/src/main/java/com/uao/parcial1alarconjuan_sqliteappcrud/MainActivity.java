4437e15ecc7ea2f947d365acfcb8232d3a9971a3
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-08-30 18:33:09 -0500

    corrección de errores

--- app/src/main/java/com/uao/parcial1alarconjuan_sqliteappcrud/MainActivity.java
+++ app/src/main/java/com/uao/parcial1alarconjuan_sqliteappcrud/MainActivity.java
@@ -16,111 +16,97 @@ import com.uao.parcial1alarconjuan_sqliteappcrud.sqlConn.AdminSQLiteOpenHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText cedula,nombreCompleto,institucion,mesa;
-    private Button btnCreate,btnRead,btnUpdate,btnDelete;
+    private EditText cedula, nombreCompleto, institucion, mesa;
+    private Button btnCreate, btnRead, btnUpdate, btnDelete;
+    AdminSQLiteOpenHelper sqlAdmin = new AdminSQLiteOpenHelper(this, "uaoDB.sqlite", null, 1);
+    SQLiteDatabase db = sqlAdmin.getWritableDatabase();
+    ContentValues register = new ContentValues();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AdminSQLiteOpenHelper sqlAdmin = new AdminSQLiteOpenHelper(this,"uaoDB.sqlite",null,1);
-        SQLiteDatabase db = sqlAdmin.getWritableDatabase();
-        ContentValues register = new ContentValues();
+
         cedula = findViewById(R.id.cedula);
         nombreCompleto = findViewById(R.id.nombreCompleto);
         institucion = findViewById(R.id.institucion);
         mesa = findViewById(R.id.mesa);
+    } //fin OnCreate
+        //CRUD Operations
+        public void create (View v){
+            int txtcedula, txtmesa;
+            String txtnombreCompleto, txtinstitucion;
+            txtcedula = Integer.parseInt(cedula.getText().toString());
+            txtnombreCompleto = nombreCompleto.getText().toString();
+            txtinstitucion = institucion.getText().toString();
+            txtmesa = Integer.parseInt(mesa.getText().toString());
+            register.put("cedula", txtcedula);
+            register.put("nombre", txtnombreCompleto);
+            register.put("colegio", txtinstitucion);
+            register.put("nromesas", txtmesa);
+            try {
+                db.insert("votantes", null, register);
+                //   db.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
 
-        btnCreate = findViewById(R.id.btnCreate);
-        btnRead = findViewById(R.id.btnRead);
-        btnUpdate = findViewById(R.id.btnUpdate);
-        btnDelete = findViewById(R.id.btnDelete);
+            cedula.setText("");
+            nombreCompleto.setText("");
+            institucion.setText("");
+            mesa.setText("");
+            Toast.makeText(this, "Se realizó el registro exitosamente", Toast.LENGTH_SHORT).show();
+        }
+        public void read (View v){
+            int ced = Integer.parseInt(cedula.getText().toString());
+            try {
+                Cursor row = db.rawQuery("select nombre,colegio,nromesa from votantes where cedula=" + ced, null);
+                if (row.moveToFirst()) {
+                    nombreCompleto.setText(row.getString(0));
+                    institucion.setText(row.getString(1));
+                    mesa.setText(row.getString(2));
+                } else {
+                    Toast.makeText(this, "No se encontraron registros con ese documento.", Toast.LENGTH_SHORT).show();
 
-        btnCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-            create(v,db,register); } });
-        btnRead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                read(v,db); } });
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                update(v,db,register); } });
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                delete(v,db); } });
-    }
-
-    //CRUD Operations
-    public void create(View v,SQLiteDatabase db, ContentValues register){
-        int txtcedula,txtmesa;
-        String txtnombreCompleto,txtinstitucion;
-        txtcedula =  Integer.parseInt(cedula.getText().toString());
-        txtnombreCompleto = nombreCompleto.getText().toString();
-        txtinstitucion = institucion.getText().toString();
-        txtmesa = Integer.parseInt(mesa.getText().toString());
-        register.put("cedula",txtcedula);
-        register.put("nombre",txtnombreCompleto);
-        register.put("colegio",txtinstitucion);
-        register.put("nromesas",txtmesa);
-        try {
-            db.insert("votantes",null,register);
-         //   db.close();
-        }catch (SQLException e){
-            System.out.println(e);
+                }
+                //    db.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        public void update (View v){
+            int txtcedula, cant, txtmesa;
+            String txtnombreCompleto, txtinstitucion;
+            txtcedula = Integer.parseInt(cedula.getText().toString());
+            txtnombreCompleto = nombreCompleto.getText().toString();
+            txtinstitucion = institucion.getText().toString();
+            txtmesa = Integer.parseInt(mesa.getText().toString());
+            register.put("nombre", txtnombreCompleto);
+            register.put("colegio", txtinstitucion);
+            register.put("nromesas", txtmesa);
+            cant = db.update("votantes", register, "cedula=" + txtcedula, null);
+            //    db.close();
+            if (cant > 0) {
+                Toast.makeText(this, "Se modificaron los registros con ese documento.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "No se encontraron registros con ese documento.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        public void delete (View v){
+            int txtcedula = Integer.parseInt(cedula.getText().toString());
+            int cant = db.delete("votantes", "cedula=" + txtcedula, null);
+            //     db.close();
+            cedula.setText("");
+            nombreCompleto.setText("");
+            institucion.setText("");
+            mesa.setText("");
+            if (cant > 0) {
+                Toast.makeText(this, "Se eliminó el registro con ese documento.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "No se encontraron registros con ese documento.", Toast.LENGTH_SHORT).show();
+            }
         }
 
-        cedula.setText("");nombreCompleto.setText("");institucion.setText("");mesa.setText("");
-        Toast.makeText(this,"Se realizó el registro exitosamente",Toast.LENGTH_SHORT).show();
-    }
-    public void read(View v,SQLiteDatabase db){
-        int ced = Integer.parseInt(cedula.getText().toString());
-        try{
-        Cursor row = db.rawQuery("select nombre,colegio,nromesa from votantes where cedula="+ced,null);
-        if(row.moveToFirst()) {
-            nombreCompleto.setText(row.getString(0));
-            institucion.setText(row.getString(1));
-            mesa.setText(row.getString(2));
-        }else{
-            Toast.makeText(this,"No se encontraron registros con ese documento.",Toast.LENGTH_SHORT).show();
-
-        }
-    //    db.close();
-        }catch (SQLException e){
-            System.out.println(e);
-            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
-        }
-    }
-    public void update(View v,SQLiteDatabase db, ContentValues register) {
-        int txtcedula,cant,txtmesa;
-        String txtnombreCompleto,txtinstitucion;
-        txtcedula = Integer.parseInt(cedula.getText().toString());
-        txtnombreCompleto = nombreCompleto.getText().toString();
-        txtinstitucion = institucion.getText().toString();
-        txtmesa =Integer.parseInt( mesa.getText().toString());
-        register.put("nombre",txtnombreCompleto);
-        register.put("colegio",txtinstitucion);
-        register.put("nromesas",txtmesa);
-        cant = db.update("votantes",register,"cedula="+txtcedula,null);
-    //    db.close();
-        if(cant>0) {
-            Toast.makeText(this,"Se modificaron los registros con ese documento.",Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this,"No se encontraron registros con ese documento.",Toast.LENGTH_SHORT).show();
-        }
-    }
-    public void delete(View v,SQLiteDatabase db){
-        int txtcedula = Integer.parseInt(cedula.getText().toString());
-        int cant = db.delete("votantes","cedula="+txtcedula,null);
-   //     db.close();
-        cedula.setText("");nombreCompleto.setText("");institucion.setText("");mesa.setText("");
-        if(cant>0){
-            Toast.makeText(this,"Se eliminó el registro con ese documento.",Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this,"No se encontraron registros con ese documento.",Toast.LENGTH_SHORT).show();
-        }
-    }
 }
+
